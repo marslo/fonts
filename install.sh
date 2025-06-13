a1bc300a927e2a25f48598699651775164f05565
marslo <marslo.jiao@gmail.com>
2025-06-12 17:25:22 -0700

    feat: added more new fonts into install.sh
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- install.sh
+++ install.sh
@@ -17,22 +17,39 @@ else
 fi
 
 # sans
-cp -f Candara/*NerdFont* "${fontPath}"
-cp -f Gisha/*NerdFont* "${fontPath}"
-cp -f Titillium/*NerdFont* "${fontPath}"
-cp -f Grandstander/*NerdFont* "${fontPath}"
-cp -f Recursive/*_Desktop_NF/*/*SansCasual*NerdFont-*.otf "${fontPath}"
-cp -f Operator/*ProNF/*NerdFont*.otf "${fontPath}"
+cp -f Candara/*NerdFont*                                     "${fontPath}"
+cp -f Gisha/*NerdFont*                                       "${fontPath}"
+cp -f Titillium/*NerdFont*                                   "${fontPath}"
+cp -f Grandstander/*NerdFont*                                "${fontPath}"
+cp -f Recursive/*DesktopNF/*/*SansCasual*NerdFont-*.otf      "${fontPath}"
+cp -f Operator/*ProNF/otf/*NerdFont*.otf                     "${fontPath}"
+cp -f NotoSansSC/*NerdFont*                                  "${fontPath}"
+cp -f spleen/*NerdFont*.otf                                  "${fontPath}"
+cp -f Orbitron/*NerdFont*                                    "${fontPath}"
+cp -f msyh/*NerdFont*.otf                                    "${fontPath}"
 
 # mono
-cp -f {VictorMono,ComicMono}/*NerdFont*.otf "${fontPath}"
-cp -f monofur/*NerdFont*.ttf "${fontPath}"
-cp -f Monaco/*NF/otf/*.otf "${fontPath}"
-cp -f Operator/*Mono*NF/otf/*.otf "${fontPath}"
-cp -f Recursive/Recursive_Code_NF/RecMonoCasual/*NerdFont*.otf "${fontPath}"
+cp -f {VictorMono,ComicMono}/*NerdFont*.otf                  "${fontPath}"
+cp -f monofur/*NerdFont*.ttf                                 "${fontPath}"
+cp -f Monaco/*NF/otf/*.otf                                   "${fontPath}"
+cp -f menlo/*NerdFont*.otf                                   "${fontPath}"
+cp -f Operator/*Mono*NF/otf/*.otf                            "${fontPath}"
+cp -f Recursive/RecursiveCodeNF/RecMonoCasual/*NerdFont*.otf "${fontPath}"
+cp -f audiolink/mono/*NerdFont*.otf                          "${fontPath}"
+cp -f monaspace/radon/*NerdFont*.otf                         "${fontPath}"
+cp -f Lekton/*NerdFont*                                      "${fontPath}"
+cp -f agave/*NerdFont*                                       "${fontPath}"
+
+# cn
+cp -f QianLiJiangShan/*NerdFont*                             "${fontPath}"
+cp -f LXGW-WenKai/mono/*NerdFont*.otf                        "${fontPath}"
+cp -f LXGW-WenKai/sans/*NerdFont*                            "${fontPath}"
+cp -f Yozai/*NerdFont*                                       "${fontPath}"
+cp -f Shayufeite/*NerdFont*                                  "${fontPath}"
 
 # handwriting
-cp -f segoe-print/*NerdFont* "${fontPath}"
-cp -f Papyrus/*NerdFont* "${fontPath}"
+cp -f segoe-print/*NerdFont*                                 "${fontPath}"
+cp -f Papyrus/*NerdFont*                                     "${fontPath}"
+cp -f BradleyHandITC/*NerdFont*                              "${fontPath}"
 
-# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh
+# vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh:
